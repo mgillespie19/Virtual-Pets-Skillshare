43f7a47dbae5244127974acfdfa5d1e8f832f5f0
Max Gilespie <max.gillespie10@gmail.com>
2020-09-07 16:52:19 -0400

    change name of root view from ContentView to PetView

--- Virtual-Pets-Skillshare/Virtual-Pets-Skillshare/PetView.swift
+++ Virtual-Pets-Skillshare/Virtual-Pets-Skillshare/PetView.swift
@@ -8,14 +8,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PetView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Pet view")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct PetView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        PetView()
     }
 }
